8938891fe50aec9c335f97118354a5891e217687
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-12-07 18:58:17 +0700

    add cors

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-require('dotenv').config()
-const express = require('express')
-const PORT = 3600
-const db = require('./models')
-const { userRouter, absenRouter, salaryRouter, jadwalRouter } = require('./routers')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const PORT = 3600;
+const db = require('./models');
+const { userRouter, absenRouter, salaryRouter, jadwalRouter } = require('./routers');
+const cors = require('cors');
 
-const app = (express())
-app.use(express.json())
+const app = express();
 
-app.use('/user', userRouter)
-app.use('/absen', absenRouter)
-app.use('/salary', salaryRouter)
-app.use('/jadwal', jadwalRouter)
-app.use(express.static('./public'))
+app.use(cors({
+  origin: '*',
+}));
+app.use(express.json());
+
+app.use('/user', userRouter);
+app.use('/absen', absenRouter);
+app.use('/salary', salaryRouter);
+app.use('/jadwal', jadwalRouter);
+app.use(express.static('./public'));
 
 app.listen(PORT, () => {
     // db.sequelize.sync({alter : true})
     console.log(`server is running at port : ${PORT}`);
-})
+});
