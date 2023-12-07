3aad8de60495d03ab67038f55e3d0d15470ffdd0
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-12-07 18:30:00 +0700

    change config.js

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.USERNAME_DATABASE,
-    password: process.env.PASSWORD_DATABASE,
-    database: process.env.DATABASE_NAME,
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: "sql12668401",
+    password: "Zg2bvSR6Fj",
+    database: "sql12668401",
+    host: "sql12.freesqldatabase.com",
+    dialect: "mysql",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
-}
+    username: "sql12668401",
+    password: "Zg2bvSR6Fj",
+    database: "sql12668401",
+    host: "sql12.freesqldatabase.com",
+    dialect: "mysql",
+  },
+};
--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 require('dotenv').config()
 const express = require('express')
-const PORT = 2000
+const PORT = 3307
 const db = require('./models')
 const { userRouter, absenRouter, salaryRouter, jadwalRouter } = require('./routers')
-const cors = require('cors');
 
 const app = (express())
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  }));
 
 app.use('/user', userRouter)
 app.use('/absen', absenRouter)
