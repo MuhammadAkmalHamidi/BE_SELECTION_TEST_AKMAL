83f5d3b98ec0a10eff6c0383a755392d55eee85f
MuhammadAkmalHamidi <hamidiakmal193@gmail.com>
2023-12-10 17:15:41 +0700

    add cors

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
+app.use(cors({ origin: 'https://appsensi-nine.vercel.app' }));
 
 app.use(express.json());
 
